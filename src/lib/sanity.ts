623eabb068defbd80f5587bf12b090cf7460e747
Ben Lattimore <hello@benlattimore.com>
2024-10-14 16:33:24 +0100

    Initial commit: Set up Next.js project with TypeScript and Tailwind

---
+++ src/lib/sanity.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+
+export const client = createClient({
+  projectId: '9cci858w',
+  dataset: 'production',
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+})
+
+// Set up image builder
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source)
+}
